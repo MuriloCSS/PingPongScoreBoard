a391806fe1669ff19aee24378bb4137ac88cba54
Murilo Corrêa <murilosao06@gmail.com>
2026-09-01 17:47:02 -0300

    feat: add abordagem ViewModel + SavedStateHandle

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-| Abordagem                  |      Rotação de Tela       |      Morte do Processo      |
-|:---------------------------|:--------------------------:|:---------------------------:|
-| remember                   |       Não sobrevive        |        Não sobrevive        |
-| ViewModel + mutableStateOf |         Sobrevive          |        Não sobrevive        |
-| ViewModel + StateFlow      |         Sobrevive          |        Não sobrevive        |
+| Abordagem                    |      Rotação de Tela       |      Morte do Processo      |
+|:-----------------------------|:--------------------------:|:---------------------------:|
+| remember                     |       Não sobrevive        |        Não sobrevive        |
+| ViewModel + mutableStateOf   |         Sobrevive          |        Não sobrevive        |
+| ViewModel + StateFlow        |         Sobrevive          |        Não sobrevive        |
+| ViewModel + SavedStateHandle | Sobrevive  | Sobrevive  |
--- app/src/main/java/br/edu/ifsp/scl/sc3038467/pingpongscoreboard/PingPongViewModel.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/pingpongscoreboard/PingPongViewModel.kt
@@ -3,31 +3,34 @@ package br.edu.ifsp.scl.sc3038467.pingpongscoreboard
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.savedstate.savedState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class PingPongViewModel : ViewModel(){
+class PingPongViewModel(
+    private val savedStateHandle: SavedStateHandle ) : ViewModel(){
 
-    private val _pontosA = MutableStateFlow(0)
-    val pontosA: StateFlow<Int> = _pontosA.asStateFlow()
 
-    private val _pontosB = MutableStateFlow(0)
-    val pontosB: StateFlow<Int> = _pontosB.asStateFlow()
+    val pontosA: StateFlow<Int> = savedStateHandle.getStateFlow("pontosA", 0)
+    val pontosB: StateFlow<Int> = savedStateHandle.getStateFlow("pontosB", 0)
 
 
     fun addPontoA(){
-        _pontosA.value++
+        val placarAtual = savedStateHandle.get<Int>("pontosA") ?: 0
+        savedStateHandle["pontosA"] = placarAtual + 1
     }
 
     fun addPontoB(){
-        _pontosB.value++
+        val placarAtual = savedStateHandle.get<Int>("pontosB") ?: 0
+        savedStateHandle["pontosB"] = placarAtual + 1
     }
 
     fun resetJogo(){
-        _pontosA.value = 0
-        _pontosB.value = 0
+        savedStateHandle["pontosA"] = 0
+        savedStateHandle["pontosB"] = 0
     }
 
 }
